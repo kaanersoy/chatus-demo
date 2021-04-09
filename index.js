7e3590ca455ee0726fa51730c3c760db6d627460
Kaan Ersoy <kaanersoy12@gmail.com>
2021-04-09 19:41:06 +0300

    Remove cookie parser and sessions

--- index.js
+++ index.js
@@ -1,47 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const { mainRouter } = require('./routes/main-router');
 const io = require('socket.io')(http);
-const cookieParser = require('cookie-parser');
-const { sessionWithOptions } = require('./helpers/session');
+const authRoute = require('./routes/auth.js');
 
 // Middlewares
 app.use(require('body-parser').json());
-app.use(cookieParser());
-app.use(sessionWithOptions);
-
 // Serve html files
 app.use(express.static('public'));
 
-app.use('/auth', mainRouter);
-
-app.get('/chat', (req, res) => {
-  if (!req.session.username_session) {
-    return res.redirect('/');
-  }
-  res.sendFile(__dirname + '/public/chat.html');
-});
-
-let activeUsers = [];
+app.use('/auth', authRoute);
 
 io.on('connection', (socket) => {
   const user = {
-    username: socket.handshake.auth.username,
+    username: socket.handshake.auth.username
   };
-  activeUsers.push(user);
-  const sendRequest = { usercount: activeUsers.length, users: activeUsers };
-
-  socket.emit('user_count', sendRequest);
-  socket.broadcast.emit('user_count');
-
-  socket.on('disconnect', () => {
-    activeUsers = activeUsers.filter(
-      (user) => user.username == socket.handshake.auth.username
-    );
-    console.log(activeUsers);
-    socket.broadcast.emit('user_count', sendRequest);
-  });
 });
 
 http.listen(3000, () => {
